58689a36ff2ddc6c0ca033ed040695e678df1998
Amir <64017570+AmirAshtariG@users.noreply.github.com>
2025-04-13 21:42:07 +0200

    create ObjectDetectionYoursVideo.sh lesson2

--- Month3/Lesson2/yolo/ObjectDetectionYours.sh
+++ Month3/Lesson2/yolo/ObjectDetectionYours.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+imagepass="${1:-$(pwd)/samples/image.jpg}"
+echo "$imagepass"
 # Step 1: Activate the virtual environment
 echo "Activating virtual environment..."
 source yolo11_env/bin/activate
@@ -9,10 +11,12 @@ yolo export model=yolo11n.pt
 
 # Step 3: Run predication using yollo
 echo "Predication start..."
-yolo predict model='yolo11n.torchscript' source='path/to/video.mp4'
+yolo predict model='yolo11n.torchscript' source=$imagepass
 
 # Step 4: Deactivate the virtual environment
 echo "Deactivating virtual environment..."
 deactivate
 
 echo "YOLOv11 predication example complete."
+
+
